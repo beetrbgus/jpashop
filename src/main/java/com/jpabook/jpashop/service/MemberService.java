6f1ee6e66cb0609be1366f61771ec42d3526ea21
beetrbgus <devbeetrb0214@gmail.com>
2022-07-28 00:46:50 +0900

    feat - member 를 이용한 저장

--- src/main/java/com/jpabook/jpashop/service/MemberService.java
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -4,5 +4,6 @@ import com.jpabook.jpashop.dto.MemberJoinDto;
 import com.jpabook.jpashop.domain.Member;
 
 public interface MemberService {
-    public Member saveMember(MemberJoinDto memberJoinDto);
+    public Member saveMember(Member member);
+
 }
