6cae55bc04ece65d3a465298202d5ed27dd03b8d
beetrbgus <devbeetrb0214@gmail.com>
2022-08-08 23:55:27 +0900

    feat - MemberApiController 추가 - Hibernate5Module Bean 설정

--- src/main/java/com/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/com/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package com.jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +12,11 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+	// 실무에서 사용하기는 좀 그럼 (굳이 사용?)
+	@Bean
+	Hibernate5Module hibernate5Module(){
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true); // Lazy Loading을 강제로 가져옴. 사용 X
+		return hibernate5Module;
+	}
 }
